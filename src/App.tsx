6afa70840f8dc58ca73d24c1034514d82c691d84
Igor Tkachenko <igor.w.tkachenko@gmail.com>
2024-01-15 23:08:59 +0100

    Implemented mobile socket connection test

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,24 @@ import { DebugSSIView } from '@owlmeans/vc-lib-native/dist/debug'
 import { NavigationContainer } from '@react-navigation/native'
 import { buildUIExtensionRegistry } from '@owlmeans/vc-lib-react/dist/shared'
 import { buildCommNativeExtension } from '@owlmeans/vc-ext-comm/dist/native'
-import { commConfig } from './config'
+import { buildIdentityExtensionNative } from '@owlmeans/vc-ext-identity/dist/native'
+import { commConfig, config } from './config'
 
+const EXAMPLE_IDENTITY_TYPE = 'ExampleIdentity'
 const registry = buildUIExtensionRegistry()
 
 registry.registerSync(buildCommNativeExtension(commConfig))
-
+registry.registerSync(buildIdentityExtensionNative(EXAMPLE_IDENTITY_TYPE, { appName: config.name }, {
+  name: '',
+  code: 'example-identity',
+  organization: 'Example Org.',
+  home: 'https://my-example.org/',
+  schemaBaseUrl: 'https://my-example.org/schemas/'
+}))
 
 function App(): JSX.Element {
   return <NavigationContainer>
-    <DebugSSIView extensions={registry} />
+    <DebugSSIView extensions={registry.normalize()} />
   </NavigationContainer>
 }
 
